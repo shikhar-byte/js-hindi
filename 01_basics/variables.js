9663965a996b088d07c7f2a5ab2957fe4e380d5c
Shikhar Kumar <50840670+shikhar-byte@users.noreply.github.com>
2022-12-30 05:37:40 +0000

    variables config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 14425;
+let accountEmail = "shikhar@google.com";
+var accountPassword = "12345";
+accountCity =" Gurgaon";
+let accountState;
+
+// accountId=2;
+accountEmail="hc@google.com";
+accountPassword="45345";
+accountCity="banglore";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
